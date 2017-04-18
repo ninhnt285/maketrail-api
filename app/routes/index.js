d46545e60cc4a3da26da605e50fd8d0fbb3c32d8
anhhoang236 <hoangtran2306@gmail.com>
2017-04-18 12:18:26 +0700

    minor fix

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,7 +5,12 @@ import graphQLRoutes from './graphql';
 
 const router = express.Router();
 
-router.use(cors());
+router.use(cors())
+
+// Test URL
+router.get('/', (req, res) => {
+  res.send('Hello MakeTrail');
+});
 
 // GraphQL Server
 router.use('/graphql', graphQLRoutes);
