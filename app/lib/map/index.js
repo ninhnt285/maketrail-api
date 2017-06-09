9b20d66642fbaa6e6576f86f89b0d7ba40fad7dd
nick <salem280575@gmail.com>
2017-06-09 12:38:47 -0400

    Export visited places to PNG Map

--- app/lib/map/index.js
+++ app/lib/map/index.js
@@ -1,7 +1,39 @@
+import fs from 'fs';
+import xml2js from 'xml2js';
+import gm from 'gm';
+import path from 'path';
 import request from 'request-promise';
 import { GOOGLE_API_KEY } from '../../config';
 
 import CountryModel from '../../database/models/country';
+
+const im = gm.subClass({ imageMagick: true });
+
+function xmlToJson(data) {
+  const parser = new xml2js.Parser();
+  return new Promise(function(resolve, reject) {
+    parser.parseString(data.substring(0, data.length), function(err, json) {
+      if (err) {
+        reject(err);
+      }
+      resolve(json);
+    });
+  });
+}
+
+function convertSvgToPng(fileName) {
+  return new Promise(function(resolve, reject) {
+    im(path.join(__dirname, `../../../static/maps/userSvg/${fileName}`))
+      .density(100)
+      .trim()
+      .write(path.join(__dirname, `../../../static/maps/userPng/${fileName.replace('.svg', '.png')}`), function(err) {
+        if (err) {
+          reject(err);
+        }
+        resolve();
+      });
+  });
+}
 
 export async function convertGeoToId(lat, long) {
   try {
@@ -40,6 +72,56 @@ export async function convertGeoToId(lat, long) {
     }
 
     return svgIds;
+  } catch(err) {
+    console.log(err);
+    return false;
+  }
+}
+
+export async function drawUserMap(userId) {
+  try {
+    /* TODO: Find countries and set color
+    * Status:
+    * 0: Not visited    #999999
+    * 1: Visited        #3c5bdc
+    * 2: In a Plan      #330000
+    */
+
+    // stroke="#000000" fill-opacity="0.8" stroke-opacity="0.5"
+    const countries = {
+      'SG': { status: 1 },
+      'MY': { status: 1 },
+      'US': { status: 1 },
+      'VN': { status: 1 }
+    };
+
+    // Build SVG File
+    let fileData = fs.readFileSync(path.join(__dirname, '../../../static/maps/userSvg/worldHigh.svg'), 'utf8');
+    const oSvgData = await xmlToJson(fileData);
+
+    // Add colors to SVG File
+    const items = oSvgData.svg.g[0].path;
+    for (let itemTemp of items) {
+      const item = itemTemp['$'];
+      let color = '#999999';
+      if (countries[item.id]) {
+        if (countries[item.id].status === 1) {
+          color = '#3c5bdc';
+        } else {
+          color = '#330000';
+        }
+      }
+
+      fileData = fileData.replace(`<path id="${item.id}" `, `<path id="${item.id}" fill="${color}" stroke="#000000" fill-opacity="0.8" stroke-opacity="0.5" `);
+    }
+
+    // Save SVG File
+    fs.writeFileSync(path.join(__dirname, `../../../static/maps/userSvg/${userId}.svg`), fileData);
+
+    // Convert SVG to PNG
+    await convertSvgToPng(`${userId}.svg`);
+    console.log('Completed!');
+
   } catch(err) {
     console.log(err);
     return false;
--- app/scripts/testMapFinder.js
+++ app/scripts/testMapFinder.js
@@ -1,29 +1,32 @@
 import connectDb from '../database/connectDb';
-import { convertGeoToId } from '../lib/map';
+import { convertGeoToId, drawUserMap } from '../lib/map';
 
 async function onConnected() {
-  // Ninh Bình, Vietnam - VN
-  let areaSvgId = await convertGeoToId(20.192269, 105.928889);
-  console.log(areaSvgId);
-  console.log('-----');
-  areaSvgId = await convertGeoToId(20.192269, 105.928889, 'VN');
-  console.log(areaSvgId);
-  console.log('-----');
+  // // Ninh Bình, Vietnam - VN
+  // let areaSvgId = await convertGeoToId(20.192269, 105.928889);
+  // console.log(areaSvgId);
+  // console.log('-----');
+  // areaSvgId = await convertGeoToId(20.192269, 105.928889, 'VN');
+  // console.log(areaSvgId);
+  // console.log('-----');
+  //
+  // // Singapore - SG
+  // areaSvgId = await convertGeoToId(1.377526, 103.997360);
+  // console.log(areaSvgId);
+  // console.log('-----');
+  //
+  // // Heard Island and McDonald Islands - HM
+  // areaSvgId = await convertGeoToId(-53.078850, 73.580934);
+  // console.log(areaSvgId);
+  // console.log('-----');
+  //
+  // // North Carolina, USA - NC, US
+  // areaSvgId = await convertGeoToId(35.721562,-76.891129);
+  // console.log(areaSvgId);
+  // console.log('-----');
 
-  // Singapore - SG
-  areaSvgId = await convertGeoToId(1.377526, 103.997360);
-  console.log(areaSvgId);
-  console.log('-----');
-
-  // Heard Island and McDonald Islands - HM
-  areaSvgId = await convertGeoToId(-53.078850, 73.580934);
-  console.log(areaSvgId);
-  console.log('-----');
-
-  // North Carolina, USA - NC, US
-  areaSvgId = await convertGeoToId(35.721562,-76.891129);
-  console.log(areaSvgId);
-  console.log('-----');
+  // Draw User Map
+  await drawUserMap('12345');
 }
 
 connectDb(onConnected);
