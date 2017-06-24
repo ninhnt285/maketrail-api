8256cd1227cfeeded0efd72426ac2d68aff58116
hien <lehien271@gmail.com>
2017-06-25 01:20:31 +0700

    add viewer in mutations/trip/delete

--- app/graphql/mutations/trip/delete.js
+++ app/graphql/mutations/trip/delete.js
@@ -14,6 +14,7 @@ import TripService from '../../../database/helpers/trip';
 import TripType from '../../types/trip';
 import { TripEdge } from '../../connections/trip';
 import { edgeFromNode } from '../../../lib/connection';
+import viewer from '../../queries/viewer';
 
 const DeleteTripMutation = mutationWithClientMutationId({
   name: 'DeleteTrip',
@@ -40,7 +41,12 @@ const DeleteTripMutation = mutationWithClientMutationId({
     edge: {
       type: TripEdge,
       resolve: ({ item }) => edgeFromNode(item)
-    }
+    },
+    deletedId: {
+      type: GraphQLID,
+      resolve: ({ deletedId }) => deletedId
+    },
+    viewer
   },
 
   mutateAndGetPayload: async ({ id }, { user }) => {
@@ -53,7 +59,8 @@ const DeleteTripMutation = mutationWithClientMutationId({
     }
     return {
       success: true,
-      item: res.item
+      item: res.item,
+      deletedId: id
     };
   }
 });
